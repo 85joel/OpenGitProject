358b643d55f5d10f437449f9e2259a072ebf9658
Administrator <Administrator@ACADEMY-6162X31>
2016-07-07 10:01:54 +0200

    Add project files.

---
+++ OpenGitProject/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenGitProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is openSource");
+        }
+    }
+}
